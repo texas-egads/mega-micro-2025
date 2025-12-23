280254ac7769fb9894c16ab667ab68505f6d82cc
AadiTakle <aadiatakle@gmail.com>
2025-12-22 23:17:03 -0600

    Basic Functioning Minigame Implementation

--- Assets/5-TeamShakra/Scripts/NailController.cs
+++ Assets/5-TeamShakra/Scripts/NailController.cs
@@ -1,17 +1,119 @@
+using UnityEditor.UI;
 using UnityEngine;
 
 
-public class NailHandler : MonoBehaviour
+namespace TeamShakra
 {
+  public class NailHandler : MonoBehaviour
+  {
+    [SerializeField] private float HAMMER_PULL_DISTANCE;
+    [SerializeField] private int PULLS_NEEDED;
+    [SerializeField] private float FLY_TIME;
+    [SerializeField] private float SPIN_SPEED; // degrees per second
+    private GameObject nailDest;
+    private GameController gameController;
+    private int hammerPulls = 0;
+    private bool isFlying = false;
+    private float flyTimer = 0f;
+    private Vector3 startPosition;
+    private Vector3 targetPosition;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+      nailDest = GameObject.FindWithTag("NailDestination");
+      if (nailDest == null)
+      {
+        Debug.LogError("Nail Destination not found");
+      }
+      gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+      if (gameController == null)
+      {
+        Debug.LogError("Game Controller not found");
+      }
     }
 
     // Update is called once per frame
     void Update()
     {
+      if (isFlying)
+      {
+        flyTimer += Time.deltaTime;
+        float t = flyTimer / FLY_TIME;
         
+        if (t >= 1f)
+        {
+          // Reached destination, destroy the nail
+          gameController.removeNail();
+          Destroy(gameObject);
+          return;
+        }
+        
+        // Linear interpolation for horizontal movement
+        Vector3 currentPos = Vector3.Lerp(startPosition, targetPosition, t);
+        
+        // Add parabolic arc (height based on sine wave)
+        float arcHeight = Mathf.Sin(t * Mathf.PI) * Vector3.Distance(startPosition, targetPosition) * 0.5f;
+        currentPos.y += arcHeight;
+        
+        transform.position = currentPos;
+        
+        // Spin the nail
+        transform.Rotate(0, 0, SPIN_SPEED * Time.deltaTime);
+      }
     }
+
+    public Vector3 hammerPull()
+    {
+      hammerPulls++;
+      if (hammerPulls == PULLS_NEEDED)
+      {
+        startNailToss();
+        return Vector3.zero;
+      }
+      Vector3 movement = new Vector3(HAMMER_PULL_DISTANCE, 0, 0);
+      transform.Translate(movement);
+      
+      //TODO: Add sound here
+      //TODO: Add wood break particles/texture here
+      
+      return movement;
+    }
+
+    void startNailToss()
+    {
+      // Remove colliders
+      Collider2D[] colliders = gameObject.GetComponents<Collider2D>();
+      foreach (Collider2D collider in colliders)
+      {
+        Destroy(collider);  
+      }
+      
+      // Initialize flight parameters
+      startPosition = transform.position;
+      targetPosition = nailDest.transform.position;
+      isFlying = true;
+      flyTimer = 0f;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+      if (other.CompareTag("Hammer"))
+      {
+        GameObject hammerObj = other.gameObject;
+        PlayerController player = hammerObj.GetComponent<PlayerController>();
+        player.intersectNail(gameObject);
+      }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+      if (other.CompareTag("Hammer"))
+      {
+        GameObject hammerObj = other.gameObject;
+        PlayerController player = hammerObj.GetComponent<PlayerController>();
+        player.removeIntersectNail();
+      }
+    }
+  }
 }
--- Assets/5-TeamShakra/Scripts/PlayerController.cs
+++ Assets/5-TeamShakra/Scripts/PlayerController.cs
@@ -6,43 +6,69 @@ using UnityEngine;
 namespace TeamShakra
 {
   public class PlayerController : MonoBehaviour
+  {
+    // All floats in * per second
+    [SerializeField] private float ROTATION_SPEED; // degrees / second
+    private float swingStartSpeed; 
+    [SerializeField] private float SWING_TIME; // seconds
+    [SerializeField] private float MOVEMENT_SPEED; // units / second
+    private float swingTimer = 0.0f;
+    private GameObject intersectingNail = null;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-      // All floats in * per second
-      [SerializeField] private const float ROTATION_SPEED = 100.0f; // degrees / second
-      private const float SWINGING_START_SPEED = 3 * ROTATION_SPEED; 
-      [SerializeField] private const float SWING_DRAG = 10.0f;
-      [SerializeField] private const float MOVEMENT_SPEED = 10.0f; // units / second
-      private float swingSpeed = 0.0f;
-
-      // Start is called once before the first execution of Update after the MonoBehaviour is created
-      void Start()
-      {
-        
-      }
-
-      // Update is called once per frame
-      void Update()
-      {
-        // Get Inputs
-        float movementInput = Input.GetAxis("Vertical") * MOVEMENT_SPEED;
-        float rotationInput = Input.GetAxis("Horizontal") * ROTATION_SPEED;
-        Debug.Log($"Rotation Input: {rotationInput}");
-        bool spacePressed = Input.GetButtonDown("Space");
-        if (spacePressed && swingSpeed <= 0.0f) {
-          swingSpeed = SWINGING_START_SPEED;
-        }
-
-        // Update Positions
-        float movementAmount = movementInput * Time.deltaTime;
-        float rotationAmount;
-        if (swingSpeed > 0.0f) {
-          swingSpeed -= SWING_DRAG;
-          rotationAmount = -1.0f * swingSpeed * Time.deltaTime;
-        } else {
-          rotationAmount = rotationInput * Time.deltaTime;
-        }
-        transform.Translate(0, movementAmount, 0);
-        transform.Rotate(0, 0, rotationAmount);
-      }
+      swingStartSpeed = 10 * ROTATION_SPEED;
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // Get Inputs
+      float movementInput = Input.GetAxis("Vertical") * MOVEMENT_SPEED;
+      // For some reason the wrong way around?
+      float rotationInput = Input.GetAxis("Horizontal") * ROTATION_SPEED * -1;
+      bool spacePressed = Input.GetButtonDown("Space");
+      if (spacePressed && swingTimer <= 0.0f) 
+      {
+        if (intersectingNail != null)
+        {
+          NailHandler nailScript = intersectingNail.GetComponent<NailHandler>();
+          Vector3 worldMovement = nailScript.hammerPull();
+          transform.position += worldMovement;
+        }
+        else
+        {
+          swingTimer = SWING_TIME;
+        }
+      }
+
+      // Update Positions
+      float movementAmount = movementInput * Time.deltaTime;
+      float rotationAmount;
+      if (swingTimer > 0.0f) 
+      {
+        swingTimer -= Time.deltaTime;
+        float swingPercentComplete = (SWING_TIME - swingTimer) / SWING_TIME;
+        float swingSpeed = Mathf.Lerp(swingStartSpeed, 0.0f, swingPercentComplete);
+        rotationAmount = -1.0f * swingSpeed * Time.deltaTime;
+      } 
+      else 
+      {
+        rotationAmount = rotationInput * Time.deltaTime;
+      }
+      transform.Translate(0, movementAmount, 0);
+      transform.Rotate(0, 0, rotationAmount);
+    }
+
+    public void intersectNail(GameObject nail)
+    {
+      intersectingNail = nail;
+    }
+
+    public void removeIntersectNail()
+    {
+      intersectingNail = null;
+    }
+  }
 }
