73be0d44d5cb6e8955ff40a3f8cbeaaf0c987e62
Zayaan <72232563+grizzledwarvet23@users.noreply.github.com>
2025-12-22 23:09:19 -0600

    backgrounds switch off

--- Assets/GameInitializer.cs
+++ Assets/GameInitializer.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameInitializer : MonoBehaviour
-{
-    [System.NonSerialized]
-    public bool isSantaWorkshop = false;
-
-    void Start()
+namespace ZABsters {
+    public class GameInitializer : MonoBehaviour
     {
-        
+        [System.NonSerialized]
+        public bool isSantaWorkshop = false;
+
+        public Sprite[] sprites;
+
+        public SpriteRenderer bgImage;
+
+        void Start()
+        {
+            //ranodmly assign isSantaWorkshop value
+            Debug.Log("WSG BRODIE");
+            isSantaWorkshop = Random.value > 0.5f;
+
+            if(isSantaWorkshop)
+            {
+                bgImage.sprite = sprites[0];
+            }
+            else
+            {
+                bgImage.sprite = sprites[1];
+            }
+        }
+
+
     }
-
-
 }
