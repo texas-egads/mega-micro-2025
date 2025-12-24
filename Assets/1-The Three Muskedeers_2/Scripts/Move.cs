19bf4bfe27f8c043fb3f87252e4d53ce12568d29
jimbo <98671167+jimjamms@users.noreply.github.com>
2025-12-24 01:34:46 -0600

    FIXED OH MY GYATT

--- Assets/1-The Three Muskedeers_2/Scripts/Move.cs
+++ Assets/1-The Three Muskedeers_2/Scripts/Move.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 namespace The_Three_Muskedeers
 {
     public class Move : MonoBehaviour
@@ -7,26 +8,40 @@ namespace The_Three_Muskedeers
         public bool canWin;
         public bool canBePressed;
 
+        [SerializeField] private Canvas canvas;
+        [SerializeField] private Camera cam;
+        Vector2 WorldObject_ScreenPosition;
+
+        [SerializeField] private float xOffset = 410;
+        [SerializeField] private float yOffset = 130;
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
             canWin = true;
-            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(GameObject.FindGameObjectWithTag("Object 2").GetComponent<RectTransform>().anchoredPosition.x - 910, GameObject.FindGameObjectWithTag("Object 2").GetComponent<RectTransform>().anchoredPosition.y, 0);
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(WorldObject_ScreenPosition.x - xOffset, WorldObject_ScreenPosition.y - yOffset);
         }
 
         // Update is called once per frame
         void Update()
         {
+            RectTransform canvasRect = canvas.GetComponent<RectTransform>();
+            Vector2 ViewportPosition = cam.WorldToViewportPoint(GameObject.FindGameObjectWithTag("Object 2").GetComponent<RectTransform>().anchoredPosition);
+            WorldObject_ScreenPosition = new Vector2(
+            ViewportPosition.x * canvasRect.sizeDelta.x,
+            ViewportPosition.y * canvasRect.sizeDelta.y);
+
             if (canWin)
             {
                 Managers.MinigamesManager.DeclareCurrentMinigameWon();
             }
-            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+            // move present
+            gameObject.GetComponent<RectTransform>().anchoredPosition += new Vector2(speed, 0) * Time.deltaTime;
             if (Input.GetKeyDown(KeyCode.Space) && canBePressed)
             {
                 Destroy(gameObject);
             }
-            if (gameObject.GetComponent<RectTransform>().anchoredPosition.x > GameObject.FindGameObjectWithTag("Object 2").GetComponent<RectTransform>().anchoredPosition.x + 400)
+            if (gameObject.GetComponent<RectTransform>().anchoredPosition.x > WorldObject_ScreenPosition.x - 7500)
             {
                 canWin = false;
                 Managers.MinigamesManager.DeclareCurrentMinigameLost();
