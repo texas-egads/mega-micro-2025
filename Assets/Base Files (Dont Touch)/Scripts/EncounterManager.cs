a0e77c6a490751b8fabcc5160dc15d63a2e28f48
RREA07 <hyzoeyu@utexas.edu>
2026-01-09 20:34:09 -0600

    Removed testing

--- Assets/Base Files (Dont Touch)/Scripts/EncounterManager.cs
+++ Assets/Base Files (Dont Touch)/Scripts/EncounterManager.cs
@@ -220,7 +220,7 @@ public class EncounterManager : MonoBehaviour
         int chance = Random.Range(0, 100); // Used to set percentages 
 
         // Check if final Boss next
-        if (encounterCount == /*maxEncounters-1*/0) //Testing
+        if (encounterCount == maxEncounters - 1)
         {
             Debug.Log("BOSS");
             return UpgradeManager.EncounterType.BOSS;
